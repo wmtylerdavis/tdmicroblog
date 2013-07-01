58fbd094e0d6424ac67a6a7a5164dc412ec890e3
Wm Tyler Davis <williamtylerdavis@gmail.com>
2013-06-30 20:19:19 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tdmicroblog::Application.config.secret_key_base = '5764d44801661a94b4f072343a89f4c16e7ff1f7c4c3fe4b6856e6476dbfe4d0a3f4c3237142aa8bc74c836b6b4fc36a70107bcec1bb52657aeea7e0629a97dc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tdmicroblog::Application.config.session_store :cookie_store, key: '_tdmicroblog_session'
