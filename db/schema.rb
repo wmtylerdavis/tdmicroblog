ec1ba8cdb52b7e6c15e8a8e08f65891aaf870890
Wm Tyler Davis <williamtylerdavis@gmail.com>
2013-07-22 21:06:25 -0400

    added rspec and validations to Users email and name

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130720110052) do
+ActiveRecord::Schema.define(version: 20130723004045) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,5 +22,7 @@ ActiveRecord::Schema.define(version: 20130720110052) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
