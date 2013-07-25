6ed4efadf11eb6568cd5524588654ab27eef892e
Wm Tyler Davis <williamtylerdavis@gmail.com>
2013-07-24 22:09:49 -0400

    Began user views, finished postgres setup and sqlite destroying

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Tdmicroblog::Application.routes.draw do
-  get "users/new"
+  resources :users
   root to: 'static_pages#home'
   get'/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
+  # get "users/new"
+  match '/signup',  to: 'users#new',            via: 'get'
 
-  get '/signup', to: 'users#new'
   # get "static_pages/home"
   # get "static_pages/help"
   # get "static_pages/about"
