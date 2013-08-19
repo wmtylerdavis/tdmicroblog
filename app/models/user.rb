7e837b857f13312e7328cb9094f63a33bc89122f
Wm Tyler Davis <williamtylerdavis@gmail.com>
2013-08-19 19:53:10 -0400

    edits :bowtie:

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ActiveRecord::Base
 				uniqueness: { case_sensitive: false }
 	has_secure_password
 	validates :password, length: { minimum: 6 }
+	validates :password_confirmation, presence: true
 
 	def User.new_remember_token
 		SecureRandom.urlsafe_base64
