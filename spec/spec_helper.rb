df669ea03d320943e1de3728285dded845e53ae4
Wm Tyler Davis <williamtylerdavis@gmail.com>
2013-08-25 16:22:47 -0400

    paginate

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -38,6 +38,8 @@ Spork.prefork do
     # automatically. This will be the default behavior in future versions of
     # rspec-rails.
     config.infer_base_class_for_anonymous_controllers = false
+
+    config.include Capybara::DSL
   end
 
 end
@@ -86,7 +88,6 @@ require 'rspec/rails'
 require 'rspec/autorun'
 require 'capybara/rspec'
 require 'capybara/rails'
-include Capybara::DSL
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
