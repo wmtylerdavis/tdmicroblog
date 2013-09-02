34a9440e4eef4427f4a20c6f7637c00fdd5def74
Wm Tyler Davis <williamtylerdavis@gmail.com>
2013-09-02 14:30:47 -0400

    finish up chapter 10 but still need to do exercises

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -21,6 +21,23 @@ describe "Static pages" do
     it { should have_selector('h1', text: 'Welcome') }
     it { should have_title(full_title('')) }
     it { should_not have_title('| Home') }
+
+    describe "for signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+      
+      before do
+        FactoryGirl.create(:micropost, user: user, content: "Blah one") 
+        FactoryGirl.create(:micropost, user: user, content: "Blah two")
+        sign_in user
+        visit root_path
+      end
+
+      it "should render the user's feed" do
+        user.feed.each do |item|
+          expect(page).to have_selector("li##{item.id}", text: item.content)
+        end
+      end
+    end
   end
 
   it "should have the right links on the layout" do
