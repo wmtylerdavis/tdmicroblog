94da6f02404437f50812b4af7de664f68dffcdf3
Wm Tyler Davis <williamtylerdavis@gmail.com>
2013-09-13 21:59:07 -0400

    ajax... jeah

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,15 +1,17 @@
 class RelationshipsController < ApplicationController
-	before_action :signed_in_user
+  before_action :signed_in_user
 
-	def create
-		@user = User.find(params[:relationship][:followed_id])
-		current_user.follow!(@user)
-		redirect_to @user
-	end
+  respond_to :html, :js
 
-	def destroy
-		@user = Relationship.find(params[:id]).followed
-		current_user.unfollow!(@user)
-		redirect_to @user
-	end
+  def create
+    @user = User.find(params[:relationship][:followed_id])
+    current_user.follow!(@user)
+    respond_with @user
+  end
+
+  def destroy
+    @user = Relationship.find(params[:id]).followed
+    current_user.unfollow!(@user)
+    respond_with @user
+  end
 end
