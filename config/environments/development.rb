084e936728ea27c90e85bbdf1ec0f543b6fc9d3b
Wm Tyler Davis <williamtylerdavis@gmail.com>
2013-09-15 11:20:09 -0400

    mailer env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,8 +38,8 @@ Tdmicroblog::Application.configure do
   address:              'smtp.gmail.com',
   port:                 587,
   domain:               'gmail.com',
-  user_name:            'williamtylerdavis@gmail.com',
-  password:             '<my password>',
+  user_name: ENV["EMAIL"],
+  password: ENV["PASSWORD"],
   authentication:       'plain',
   enable_starttls_auto: true  
   }
