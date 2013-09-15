2333def8694f5701424c3f44a6c44b65dd9b0e33
Wm Tyler Davis <williamtylerdavis@gmail.com>
2013-09-15 16:35:12 -0400

    delayed job to prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,4 +81,21 @@ Tdmicroblog::Application.configure do
   #Force all acces over SSL
   # and user secure cookies
   config.force_ssl = true
+
+    #send mail locally
+  # config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'gmail.com',
+  user_name:            ENV["EMAIL"],
+  password:             ENV["PASSWORD"],
+  authentication:       'plain',
+  enable_starttls_auto: true  
+  }
 end
